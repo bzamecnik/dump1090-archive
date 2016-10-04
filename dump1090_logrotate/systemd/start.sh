37f992da7c0d5de9de659371c1a6bcc51a6557e8
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2016-10-05 01:14:33 +0200

    Change the init script to replace shell process with python.
    Do not run it in a subprocess.

--- dump1090_logrotate/systemd/start.sh
+++ dump1090_logrotate/systemd/start.sh
@@ -4,7 +4,10 @@ PORT=30003
 INTERVAL=hour
 OUTPUT_PREFIX=data/dump1090-messages/dump1090-$(hostname)
 LOG_FILE=dump1090-logrotate.log
-/home/pi/miniconda3/bin/python3 -u dump1090-archive/dump1090_logrotate/dump1090_logrotate.py \
+# replace the shell's process
+# store stdout & stderr to a log file
+# buffer the python's output
+exec /home/pi/miniconda3/bin/python3 -u dump1090-archive/dump1090_logrotate/dump1090_logrotate.py \
   -h $HOST \
   -p $PORT \
   -i $INTERVAL \
